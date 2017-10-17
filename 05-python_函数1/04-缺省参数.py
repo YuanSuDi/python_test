17d5bd7df3dfd9244831e887971fafd6658c1388
tong19950305 <237932061@qq.com>
2017-10-17 08:46:44 +0800

    2233

--- 05-python_函数1/04-缺省参数.py
+++ 05-python_函数1/04-缺省参数.py
@@ -3,4 +3,21 @@ def test1(b, c, a=20):
     print(a)
     print(b)
     print(c)
+
+
 test1(b=22, c=33)
+
+
+def test2(a, b, *args, **kwargs):
+    print("a=%s" % a)
+    print("b=%s" % b)
+    print(args)
+    print(kwargs)
+list1 = [1, 2, 3, 4, 5, 6]
+args1 = {'name':'tom', 'age':23}
+# 在实参中*,**表示对元祖/字典进行拆包
+test2("bili", "2233", *list1, **args1)
+# 两种传参方式
+test2("元组", "we", 1, 2, 3, 4, 5, name="laownag", age=23)
+
+
