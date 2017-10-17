7ec9dc13ca7527eaf5777b77939d725df76a1af3
tong19950305 <237932061@qq.com>
2017-10-17 17:05:55 +0800

    2233

---
+++ 12-model/main.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# 导入时就意味着将模块中的代码全部执行一遍
+import send as a
+
+a.test1()
+a.test2()
+
+test = a.Test1()
+test.test11()
+
+test1 = a.Test2()
+test1.test21()
+0
---
+++ 12-model/send.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+def test1():
+    print("---test1---")
+
+
+def test2():
+    print("---test2---")
+
+
+class Test1(object):
+    def test11(self):
+        print("---test11---")
+
+
+class Test2(object):
+    def test21(self):
+        print("---test21---")
+
+# __name__变量：谁执行就会赋值为谁,如果执行本文件时，赋值为"__main__"
+if __name__ == "__main__":
+    test1()
+    test2()
